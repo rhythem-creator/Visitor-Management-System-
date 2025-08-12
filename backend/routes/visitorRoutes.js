888cc131b87bcaeab7e2b45cc120188b4deaf1cf
Rhythem <rhythem.pradhan@connect.qut.edu.au>
2025-08-12 13:00:41 +1000

    VM-6:Implement API to update visitor by ID

--- backend/routes/visitorRoutes.js
+++ backend/routes/visitorRoutes.js
@@ -6,12 +6,15 @@ const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 
 // controller functions
-const { addVisitor, getVisitors } = require('../controllers/visitorController');
+const { addVisitor, getVisitors, updateVisitor} = require('../controllers/visitorController');
 
 // VM‑4 (Create)
 router.post('/', protect, addVisitor);
 
 // VM‑5 (List)
 router.get('/', protect, getVisitors);
+
+// VM-6: update by id
+router.put('/:id', protect, updateVisitor);
 
 module.exports = router;
