1c5f538fcfa48c94cac3df82c6ab33b62d3fd7d9
Rhythem <rhythem.pradhan@connect.qut.edu.au>
2025-08-16 16:55:20 +1000

    Updated axiosConfig base URL with public IP

--- frontend/src/api/axiosConfig.jsx
+++ frontend/src/api/axiosConfig.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5001/api", // your backend base
+  baseURL: "16.176.11.91:5001/api", // your backend base
   timeout: 10000,
 });
 
